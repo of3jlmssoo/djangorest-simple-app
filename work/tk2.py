8950854dd77f8f2c07f93708121863ecc6347ae4
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-10-17 12:02:05 +0900

    tk2.py set default file

--- work/tk2.py
+++ work/tk2.py
@@ -1,21 +1,97 @@
 import tkinter as tk
+import tkinter.filedialog
+
+INITIAL_DIR = "/home/hiroshisakuma/ダウンロード/"
+INITIAL_FILE1 = "/home/hiroshisakuma/ダウンロード/Dividend Calendar - Investing.com.html"
+INITIAL_FILE2 = "/home/hiroshisakuma/ダウンロード/portf.txt"
+GEOMETRY1 = "700x600"
 
 
 class windowclass():
     def __init__(self, master):
         self.master = master
+
+        button = tk.Button(
+            root,
+            text='ファイルダイアログを開く',
+            font=(
+                '',
+                20),
+            width=24,
+            height=1,
+            bg='#999999',
+            activebackground="#aaaaaa")
+        button.bind('<ButtonPress>', self.file_dialog)
+        button.pack(pady=0)
+
+        self.file_name = tk.StringVar()
+        # self.file_name.set('未選択です')
+        self.file_name.set(INITIAL_FILE1)
+        label = tk.Label(textvariable=self.file_name, font=('', 12))
+        label.pack(pady=10)
+
+        button = tk.Button(
+            root,
+            text='ファイルダイアログを開く',
+            font=(
+                '',
+                20),
+            width=24,
+            height=1,
+            bg='#999999',
+            activebackground="#aaaaaa")
+        button.bind('<ButtonPress>', self.file_dialog2)
+        # button.bind(
+        #     '<ButtonPress>',
+        #     # self.file_dialogX(
+        #     #     INITIAL_DIR,
+        #     #     INITIAL_FILE2))
+
+        # lambda event: self.file_dialogX(event, INITIAL_DIR, INITIAL_FILE2))
+
+        button.pack(pady=0)
+
+        self.file_name2 = tk.StringVar()
+        # self.file_name2.set('未選択です')
+        self.file_name2.set(INITIAL_FILE2)
+        label = tk.Label(textvariable=self.file_name2, font=('', 12))
+        label.pack(pady=40)
+
         self.btn = tk.Button(master, text="Button", command=self.command)
         self.btn.pack()
 
     def command(self):
         self.master.withdraw()
         toplevel = tk.Toplevel(self.master)
-        toplevel.geometry("350x350")
+        # toplevel.geometry("350x350")
+        toplevel.geometry(GEOMETRY1)
+        print(self.file_name, self.file_name2)
         app = Demo2(toplevel)
+
+    def file_dialog(self, event):
+        fTyp = [("", "*")]
+        # iDir = os.path.abspath(os.path.dirname(__file__))
+        file_name = tk.filedialog.askopenfilename(
+            filetypes=fTyp, initialdir=INITIAL_DIR, initialfile=INITIAL_FILE1)
+        if len(file_name) == 0:
+            self.file_name.set('選択をキャンセルしました')
+        else:
+            self.file_name.set(file_name)
+
+    def file_dialog2(self, event):
+        fTyp = [("", "*")]
+        # iDir = os.path.abspath(os.path.dirname(__file__))
+        file_name2 = tk.filedialog.askopenfilename(
+            filetypes=fTyp, initialdir=INITIAL_DIR, initialfile=INITIAL_FILE2)
+        if len(file_name2) == 0:
+            self.file_name2.set('選択をキャンセルしました')
+        else:
+            self.file_name2.set(file_name2)
 
 
 class Demo2:
     def __init__(self, master):
+
         self.master = master
         self.frame = tk.Frame(self.master)
         self.quitButton = tk.Button(
@@ -34,7 +110,8 @@ class Demo2:
 
 root = tk.Tk()
 root.title("window")
-root.geometry("350x350")
+# root.geometry("700x1000")
+root.geometry(GEOMETRY1)
 cls = windowclass(root)
 root.mainloop()
 
