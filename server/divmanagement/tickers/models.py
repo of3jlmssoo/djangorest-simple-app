6a447255b229e4be5de202fd8eedc2b703360a73
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-11-03 23:45:46 +0900

    prepare for foreign key in dividend

--- server/divmanagement/tickers/models.py
+++ server/divmanagement/tickers/models.py
@@ -10,6 +10,53 @@ LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
+
+class Ticker(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    ticker = models.CharField(
+        max_length=10,
+        blank=False,
+        null=False,
+        unique=True)
+    vol1 = models.IntegerField(validators=[MinValueValidator(0)])
+    vol2 = models.IntegerField(validators=[MinValueValidator(0)])
+    total_vol = models.IntegerField(validators=[MinValueValidator(0)])
+    accum = models.IntegerField(validators=[MinValueValidator(0)])
+
+    owner = models.ForeignKey(
+        'auth.User',
+        related_name='tickers',
+        on_delete=models.CASCADE)
+    # highlighted = models.TextField()
+
+    # code = models.TextField()
+    # linenos = models.BooleanField(default=False)
+    # language = models.CharField(
+    #     choices=LANGUAGE_CHOICES,
+    #     default='python',
+    #     max_length=100)
+    # style = models.CharField(
+    #     choices=STYLE_CHOICES,
+    #     default='friendly',
+    #     max_length=100)
+
+    class Meta:
+        ordering = ['created']
+
+    def save(self, *args, **kwargs):
+        # """
+        # Use the `pygments` library to create a highlighted HTML
+        # representation of the code snippet.
+        # """
+        # lexer = get_lexer_by_name('Markdown')
+        # # linenos = 'table' if self.linenos else False
+        # # options = {'title': self.title} if self.title else {}
+        # formatter = HtmlFormatter(full=True)
+        # self.highlighted = highlight(self.ticker, lexer, formatter)
+        self.total_vol = self.vol1 + self.vol2
+        super(Ticker, self).save(*args, **kwargs)
+
+
 """
 確定日 : ex_date : 日付
 支払日 : pay_date : 日付
@@ -60,49 +107,3 @@ max_digits 12.12=> 4
 decimal_places => 2
 
 """
-
-
-class Ticker(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    ticker = models.CharField(
-        max_length=10,
-        blank=False,
-        null=False,
-        unique=True)
-    vol1 = models.IntegerField(validators=[MinValueValidator(0)])
-    vol2 = models.IntegerField(validators=[MinValueValidator(0)])
-    total_vol = models.IntegerField(validators=[MinValueValidator(0)])
-    accum = models.IntegerField(validators=[MinValueValidator(0)])
-
-    owner = models.ForeignKey(
-        'auth.User',
-        related_name='tickers',
-        on_delete=models.CASCADE)
-    # highlighted = models.TextField()
-
-    # code = models.TextField()
-    # linenos = models.BooleanField(default=False)
-    # language = models.CharField(
-    #     choices=LANGUAGE_CHOICES,
-    #     default='python',
-    #     max_length=100)
-    # style = models.CharField(
-    #     choices=STYLE_CHOICES,
-    #     default='friendly',
-    #     max_length=100)
-
-    class Meta:
-        ordering = ['created']
-
-    def save(self, *args, **kwargs):
-        # """
-        # Use the `pygments` library to create a highlighted HTML
-        # representation of the code snippet.
-        # """
-        # lexer = get_lexer_by_name('Markdown')
-        # # linenos = 'table' if self.linenos else False
-        # # options = {'title': self.title} if self.title else {}
-        # formatter = HtmlFormatter(full=True)
-        # self.highlighted = highlight(self.ticker, lexer, formatter)
-        self.total_vol = self.vol1 + self.vol2
-        super(Ticker, self).save(*args, **kwargs)
