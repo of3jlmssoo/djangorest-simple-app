6393465d9a1946542bb524d3aa56285777c44d8c
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-11-26 23:45:35 +0900

    project name changed

--- server/divmanagement/divmanagement/settings.py
+++ server/divmanagement/divmanagement/settings.py
@@ -48,8 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'dividends.apps.TickersConfig',
-    # 'dividends.apps.DividendsConfig',
+    'tickers.apps.TickersConfig',
+    # 'divmanagement.apps.TickersConfig',
     'django_filters',
 ]
 
--- server/divmanagement/tickers/apps.py
+++ server/divmanagement/tickers/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 # class TickersConfig(AppConfig):
 #     default_auto_field = 'django.db.models.BigAutoField'
 #     name = 'tickers'
+
+class DividendsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dividends'
+
+
+class TickersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tickers'
