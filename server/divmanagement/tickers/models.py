6a544411a44985367c598a2e6f26c4a2d1e8f643
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-12-13 11:06:56 +0900

    comments cleaned

--- server/divmanagement/tickers/models.py
+++ server/divmanagement/tickers/models.py
@@ -87,18 +87,6 @@ class Dividend(models.Model):
         'auth.User',
         related_name='dividends',
         on_delete=models.CASCADE)
-    # highlighted = models.TextField()
-
-    # code = models.TextField()
-    # linenos = models.BooleanField(default=False)
-    # language = models.CharField(
-    #     choices=LANGUAGE_CHOICES,
-    #     default='python',
-    #     max_length=100)
-    # style = models.CharField(
-    #     choices=STYLE_CHOICES,
-    #     default='friendly',
-    #     max_length=100)
 
     class Meta:
         ordering = ['created']
